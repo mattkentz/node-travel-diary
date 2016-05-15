9e43b360429ae7888bd93f3acab073a7133e0305
mattkentz <kentzia@gmail.com>
2016-05-15 12:28:01 +0200

    Changed file names and structure to follow tutorial on Scotch.io

--- config/index.js
+++
@@ -1,7 +0,0 @@
-var configValues = require('./config');
-
-module.exports = {
-  getDbConnectionString : function () {
-    return 'mongodb://' + configValues.uname + ': ' + configValues.pwd + '@ds011790.mlab.com:11790/nodetodo'
-  }
-}

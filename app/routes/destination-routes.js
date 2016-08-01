a0734dc9c231705a2eadbe80a1afa5624cf7cf3e
Matthias Kentzia <kentzia@gmail.com>
2016-08-01 18:45:54 +0200

    Add option to turn off authentication using environment variables

--- app/routes/destination-routes.js
+++ app/routes/destination-routes.js
@@ -14,35 +14,37 @@ router.get('/', DestinationController.getDestinations);
 // GET - Get a specific destination by id
 router.get('/:destination_id', DestinationController.getDestinationById);
 
-// Route middleware to verify a token
-router.use(function(req, res, next) {
+// Route middleware to verify a token - can be turned off with environment variable
+if (!process.env.IGNORE_AUTH) {
+    router.use(function(req, res, next) {
 
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+        // check header or url parameters or post parameters for token
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-    // decode token
-    if (token) {
+        // decode token
+        if (token) {
 
-        // verifies secret and checks exp
-        jwt.verify(token, app.get('auth_secret'), function(err, decoded) {
-            if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
+            // verifies secret and checks exp
+            jwt.verify(token, app.get('auth_secret'), function(err, decoded) {
+                if (err) {
+                    return res.json({ success: false, message: 'Failed to authenticate token.' });
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;
+                    next();
+                }
+            });
 
-    } else {
-        // if there is no token return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
+        } else {
+            // if there is no token return an error
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
 
-    }
-});
+        }
+    });
+}
 
 /*
  Authenticated Routes
